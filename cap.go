345013fd22f69386f02afe4389a3496983d11c16
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-08-28 11:39:55 +0300

    pre

--- cap.go
+++ cap.go
@@ -5,6 +5,7 @@ package cap
 import (
 	"errors"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/Sirupsen/logrus"
@@ -17,19 +18,21 @@ var ReconnectDelay = time.Second * 5
 var log = logrus.New()
 
 var (
-	differentConnErr = errors.New("connection has been changed")
+	DifferentConnErr = errors.New("connection has been changed")
 )
 
 type Cap struct {
 	addr string
 
-	*amqp.Connection
+	conn     *amqp.Connection
 	connReq  chan bool
 	waitConn chan chan bool
+	m        sync.Mutex
 }
 
-// Open opens a connection to amqp server
-// and re-connects if the connection drops
+// Open opens a connection to amqp server with given addr
+// and re-connects if the connection drops.
+// Returns an error if the addr is invalid
 func Open(addr string) (*Cap, error) {
 	_, err := amqp.ParseURI(addr)
 	if err != nil {
@@ -79,7 +82,9 @@ func (c *Cap) connectLoop() {
 				connected <- true
 				log.Info("connected")
 
-				c.Connection = cn
+				c.m.Lock()
+				c.conn = cn
+				c.m.Unlock()
 			}()
 
 		case is := <-connected:
@@ -120,12 +125,21 @@ func (c *Cap) RegisterSession() {
 type Channel struct {
 	*amqp.Channel
 	connAddr net.Addr
+	cap      *Cap
+}
+
+func (c *Cap) newChannel(ch *amqp.Channel) *Channel {
+	return &Channel{
+		Channel:  ch,
+		connAddr: c.conn.LocalAddr(),
+		cap:      c,
+	}
 }
 
 // AlwaysChannel calls given func with a valid channel for the first time
 // and anytime after a re-connection made to server
 func (c *Cap) AlwaysChannel(f func(*Channel)) {
-	ch, err := c.Channel()
+	ch, err := c.CreateChannel()
 	if err != nil {
 		c.AlwaysChannel(f)
 		return
@@ -137,27 +151,66 @@ func (c *Cap) AlwaysChannel(f func(*Channel)) {
 	f(ch)
 }
 
-// Channel creates a channel for the current connection.
+// CreateChannel creates a channel for the current connection.
 // It waits for a valid connection before creating a channel if there is not
-func (c *Cap) Channel() (*Channel, error) {
+func (c *Cap) CreateChannel() (*Channel, error) {
 	c.getConnReady()
-	ch, err := c.Connection.Channel()
+	c.m.Lock()
+	defer c.m.Unlock()
+	ch, err := c.conn.Channel()
 	if err != nil {
 		return nil, err
 	}
-	return &Channel{
-		Channel:  ch,
-		connAddr: c.Connection.LocalAddr(),
-	}, nil
+	return c.newChannel(ch), nil
 }
 
-// TxChannel creates a channel with Channel() but in transactional mode
-func (c *Cap) TxChannel() (*Channel, error) {
-	ch, err := c.Channel()
+// CreateTxChannel creates a channel with Channel() but in transactional mode
+func (c *Cap) CreateTxChannel() (*Channel, error) {
+	ch, err := c.CreateChannel()
 	if err != nil {
 		return nil, err
 	}
 	return ch, ch.Tx()
+}
+
+// CreateChannel creates a channel from the same connection of the channel
+// or returns an error if the connection is no longer valid
+func (ch *Channel) CreateChannel() (*Channel, error) {
+	ch.cap.m.Lock()
+	defer ch.cap.m.Unlock()
+	if ch.cap.conn.LocalAddr() == ch.connAddr {
+		chn, err := ch.cap.conn.Channel()
+		if err != nil {
+			return nil, err
+		}
+		return ch.cap.newChannel(chn), nil
+	} else {
+		return nil, DifferentConnErr
+	}
+}
+
+// CreateTxChannel creates a channel with Channel.Channel() but in transactional mode
+func (ch *Channel) CreateTxChannel() (*Channel, error) {
+	chn, err := ch.CreateChannel()
+	if err != nil {
+		return nil, err
+	}
+	return chn, chn.Tx()
+}
+
+type Delivery struct {
+	*amqp.Delivery
+	ch *Channel
+}
+
+// CreateChannel has same behavior as Channel.CreateChannel()
+func (d *Delivery) CreateChannel() (*Channel, error) {
+	return d.ch.CreateChannel()
+}
+
+// CreateTxChannel has same behavior as Channel.CreateTxChannel()
+func (d *Delivery) CreateTxChannel() (*Channel, error) {
+	return d.ch.CreateTxChannel()
 }
 
 // IsConnectionError checks the given error if it is a connection error or not
